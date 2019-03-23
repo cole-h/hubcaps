5a1a9d043eb6c81d3f18aa7c077c86d9d8c43532
Graham Christensen <graham@grahamc.com>
2019-03-22 21:08:50 -0400

    Let apps look up install IDs

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -28,6 +28,16 @@ impl<'a> App<'a> {
             MediaType::Preview("machine-man")
         )
     }
+
+    pub fn find_repo_installation<O, R>(&self, owner: O, repo: R) -> Result<Installation>
+    where
+        O: Into<String>,
+        R: Into<String> {
+        self.github.get_media::<Installation>(
+            &format!("/repos/{}/{}/installation", owner.into(), repo.into()),
+            MediaType::Preview("machine-man")
+        )
+    }
 }
 
 // representations
@@ -36,4 +46,21 @@ impl<'a> App<'a> {
 pub struct AccessToken {
     pub token: String,
     pub expires_at: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Installation {
+    pub id: i32,
+    // account: Account
+    pub access_tokens_url: String,
+    pub repositories_url: String,
+    pub html_url: String,
+    pub app_id: i32,
+    pub target_id: i32,
+    pub target_type: String,
+    // permissions: Permissions
+    pub events: Vec<String>,
+    // created_at, updated_at
+    pub single_file_name: Option<String>,
+    pub repository_selection: String,
 }
