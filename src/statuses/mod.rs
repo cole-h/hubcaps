6e3e1568ce3513cd94945fc1afad98422b667f35
Cole Helbling <cole.e.helbling@outlook.com>
2020-04-03 16:04:50 -0700

    Statuses.{created,updated}_at: optional

--- src/statuses/mod.rs
+++ src/statuses/mod.rs
@@ -65,8 +65,8 @@ impl<'a> Statuses<'a> {
 
 #[derive(Debug, Deserialize)]
 pub struct Status {
-    pub created_at: String,
-    pub updated_at: String,
+    pub created_at: Option<String>,
+    pub updated_at: Option<String>,
     pub state: State,
     pub target_url: Option<String>,
     pub description: String,
